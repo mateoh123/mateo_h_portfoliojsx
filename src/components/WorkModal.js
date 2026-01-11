af0d97bf1a6c63f2926fc12e249a500dc035980e
mateoh123 <mateoche76@gmail.com>
2026-01-11 15:16:26 -0500

    more updates

--- src/components/WorkModal.js
+++ src/components/WorkModal.js
@@ -1,29 +1,63 @@
-
+import React, { useEffect } from "react";
+import workModalBackground from "../assets/WorkModalBackground.png";
+import exitX from "../assets/exitx.png";
 
 const WorkModal = ({ isOpen, onClose, children, workTitle }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
     <div
-      className="fixed inset-0 bg-black bg-opacity-50 z-40 flex justify-end"
+      className="fixed inset-0 bg-black/80 z-50 flex justify-end overflow-hidden"
       onClick={onClose}
     >
       <div
-        className="bg-white w-full sm:w-1/2 h-full overflow-y-auto shadow-xl transform transition-transform duration-300 translate-x-0"
-        onClick={onClose}
+        className="relative w-4/6 h-screen overflow-hidden"
+        onClick={(e) => e.stopPropagation()}
       >
-        <div className="sticky top-0 bg-white z-50 border-b p-4 flex justify-between items-center">
-          <h2 className="text-3xl font-bold font-stretch-extra-expanded">
-            {workTitle}
-          </h2>
-          <button
-            onClick={onClose}
-            className="text-gray-600 hover:text-black text-2xl"
-          >
-            &times;
-          </button>
+        <img
+          src={workModalBackground}
+          alt="Modal Background"
+          className="absolute inset-0 w-full h-full object-fill"
+        />
+        <div className="absolute inset-0 z-10 flex items-center justify-center">
+          <div className="flex flex-col h-5/6 w-9/12">
+            <div className="sticky top-0 flex justify-between items-center p-1 ">
+              <h2 className="text-3xl font-bold font-stretch-extra-expanded text-black ml-4">
+                {workTitle}
+              </h2>
+              <button
+                onClick={onClose}
+                className="text-black text-3xl hover:opacity-70"
+              >
+                <img
+                  src={exitX}
+                  width="24"
+                  height="24"
+                  alt="X"
+                  className="hover:scale-125"
+                />
+              </button>
+            </div>
+            <div className="flex-1 overflow-y-auto pb-6 overscroll-contain">
+              {children}
+            </div>
+          </div>
         </div>
-        <div className="p-1">{children}</div>
       </div>
     </div>
   );
